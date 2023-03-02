77bfc74522df12602f5bc01392e09ee9a2b40143
Matthieu LEPERS <aireayquaza@hotmail.fr>
2023-03-02 23:18:10 +0100

    :bug: fix ecosystem orth missmatch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'bot',
       script: './src/index.js',
       env_production: {
-        NODE_END: 'production',
+        NODE_ENV: 'production',
       },
       env_development: {
         NODE_ENV: 'development',
